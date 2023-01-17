9fdf85aed358faa20a910237f9101d402345729e
annakrysenko <107724360+annakrysenko@users.noreply.github.com>
2023-01-17 00:06:38 +0000

    Deploying to gh-pages from @ annakrysenko/landing_with_change_themes@93222c1465077eca5feee01fe2bb7812e5c4298e 🚀

---
+++ index.9d55ccad.js
@@ -0,0 +1,2 @@
+(e=>{const t=document.querySelectorAll(".wheel__word");let l=-60;for(let o of t)o.style.transform=`rotate(${l}deg)`,l+=e})(15),(e=>{const t=document.querySelectorAll(".wheel__letter");let l=0;for(let o of t)o.style.transform=`rotate(${l}deg)`,l+=e})(10);
+//# sourceMappingURL=index.9d55ccad.js.map
