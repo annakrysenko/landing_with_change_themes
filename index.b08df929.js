a164a33af84411cc66bb084e6d43f3eec23de108
annakrysenko <107724360+annakrysenko@users.noreply.github.com>
2023-01-19 19:45:45 +0000

    Deploying to gh-pages from @ annakrysenko/landing_with_change_themes@f707fbc43e9cbd5d982f4a3603cfefc24dc977b2 🚀

---
+++ index.b08df929.js
@@ -0,0 +1,2 @@
+document.querySelector(".theme__on"),document.querySelector(".theme__reset"),document.documentElement,document.querySelector(".theme-container");
+//# sourceMappingURL=index.b08df929.js.map
